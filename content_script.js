d8c24274482c738fe3f034d539656cd531f35c72
Lars Klein <lars.klein@rwth-aachen.de>
2017-09-18 12:01:42 +0200

    updating the control flow

--- content_script.js
+++ content_script.js
@@ -1,5 +1,10 @@
-// Shorthand for $( document ).ready()
-function getSelection() {
+// this script has only one purpose: getting selected text
+// TODO: once the core features are completed, it might be nice to have
+// the translation displayed as overlay to the html page
+// that would be the job of this script too: inserting html into the DOM
+
+// function to get selected text
+function getSelectedText() {
     var focused = document.activeElement;
     var selectedText;
 
@@ -16,6 +21,8 @@ function getSelection() {
         var sel = window.getSelection();
         var selectedText = sel.toString();
     }
+
+    return selectedText;
     chrome.runtime.sendMessage({ 'text': selectedText }, function (response) {
         console.log(response.farewell);
     });
@@ -24,14 +31,23 @@ function getSelection() {
 // jquery to fire this up once the window has loaded
 $(function () {
     console.log("content script up and running");
+
     chrome.runtime.onMessage.addListener(
         function (request, sender, sendResponse) {
-            console.log(sender.tab ?
+            /*console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from somewhere else");
-            console.log(request.greeting);
-            if (request.greeting == "hello")
-                sendResponse({ farewell: "goodbye" });
+            console.log(request.greeting);*/
+
+            console.log("incoming message");
+            console.log(request);
+            
+            if(request.action == "getSelection"){
+                var selectedText = getSelectedText();
+                console.log("selectedText: "+selectedText);
+                sendResponse({ selection: selectedText });
+            }
+                
         });
 });
 
--- popup.js
+++ popup.js
@@ -2,19 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   console.log("loaded popup.js");
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {greeting: "wake up"}, function(response) {
-      console.log(response.farewell);
-    });
-  });
-
-
   $("#translation_button").click(function () {
-    alert("button clicked");
 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {greeting: "button clicked"}, function(response) {
-        console.log(response.farewell);
+      chrome.tabs.sendMessage(tabs[0].id, {action: "getSelection"}, function(response) {
+        console.log(response.selection);
       });
     });
   });
